b16061624981f16255da040289651cfdbd48d176
Samity7 <sami50737@gmail.com>
2025-06-26 16:46:21 +0300

    EmailJs added

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+  (function() {
+    emailjs.init("ujqfW8Hhqra7O_XzP"); 
+  })();
+
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    emailjs.sendForm('service_0k2iie1', 'template_x8y8z9h', this)
+      .then(function() {
+        alert('Message sent successfully!');
+      }, function(error) {
+        alert('Failed to send message: ' + JSON.stringify(error));
+      });
+  });
